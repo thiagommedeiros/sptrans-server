a42abff7909a3c9dbd46510b2bdc3aa09ce3175d
= <thiagomunhozmedeiros@gmail.com>
2017-04-16 21:41:55 -0300

    Update response headers to allow cors

--- express.js
+++ express.js
@@ -3,12 +3,13 @@ var app = express();
 var routes = require('./routes');
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
 });
+app.use(bodyParser.json());
 
 routes(app);
 
