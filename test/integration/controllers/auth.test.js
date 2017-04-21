edb06e1431cfb8e86e9e45791730076a03a01928
= <thiagomunhozmedeiros@gmail.com>
2017-04-21 00:08:38 -0300

    Add test to authController

--- test/integration/controllers/auth.test.js
+++ test/integration/controllers/auth.test.js
@@ -1,5 +1,24 @@
 import test from 'ava'
+import axios from 'axios'
 
-test('should', t => {
-  t.is(true)
-})
+const token = '1e7c20905fe86990c5227e7e9f00002fe908d4d4dd4d7c0091032dacd2d0e07d'
+
+test('should return sptransRes as true to a valid token', async t =>
+  axios({
+    method: 'post',
+    url: 'http://localhost:8888/auth',
+    params: { token }
+  }).then(res => {
+    t.is(res.data.sptransRes, true)
+  })
+)
+
+test('should return sptransRes as true to a invalid token', async t =>
+  axios({
+    method: 'post',
+    url: 'http://localhost:8888/auth',
+    params: { token: 'some-invalid-token' }
+  }).then(res => {
+    t.is(res.data.sptransRes, false)
+  })
+)
