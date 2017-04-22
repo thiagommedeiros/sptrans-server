d301e1d57c59e4fd66bbb8aeeb0a0f19475dc037
= <thiagomunhozmedeiros@gmail.com>
2017-04-22 18:53:45 -0300

    Rename data files to .json

--- test/integration/controllers/auth.test.js
+++ test/integration/controllers/auth.test.js
@@ -1,22 +1,23 @@
 import test from 'ava'
 import axios from 'axios'
 
+const url = 'http://localhost:8888/auth'
 const token = '1e7c20905fe86990c5227e7e9f00002fe908d4d4dd4d7c0091032dacd2d0e07d'
 
 test('should return sptransRes as true to a valid token', async t =>
   axios({
     method: 'post',
-    url: 'http://localhost:8888/auth',
+    url,
     params: { token }
   }).then(res => {
     t.is(res.data.sptransRes, true)
   })
 )
 
-test('should return sptransRes as false to a invalid token', async t =>
+test('should return sptransRes as false to an invalid token', async t =>
   axios({
     method: 'post',
-    url: 'http://localhost:8888/auth',
+    url,
     params: { token: 'some-invalid-token' }
   }).then(res => {
     t.is(res.data.sptransRes, false)
