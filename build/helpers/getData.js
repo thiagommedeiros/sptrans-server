fedc7dc025312d26020123c1ea063f7e03a01aef
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2017-05-30 19:49:44 -0300

    Fix getData

--- build/helpers/getData.js
+++ build/helpers/getData.js
@@ -5,13 +5,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 exports.default = function (file) {
-  return new Promise(function (resolve) {
-    var stream = _fs2.default.createReadStream('' + defaultPath + file + '.json', { encoding: 'utf8' });
-    var parser = _JSONStream2.default.parse();
-
-    stream.pipe(parser);
-
-    parser.on('data', function (obj) {
+  return new Promise(function (resolve, reject) {
+    _fs2.default.readFile('' + defaultPath + file + '.json', 'utf-8', function (err, data) {
+      if (err) reject(err);
+      var obj = JSON.parse(data);
       resolve(obj);
     });
   });
@@ -20,10 +17,6 @@ exports.default = function (file) {
 var _fs = require('fs');
 
 var _fs2 = _interopRequireDefault(_fs);
-
-var _JSONStream = require('JSONStream');
-
-var _JSONStream2 = _interopRequireDefault(_JSONStream);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
