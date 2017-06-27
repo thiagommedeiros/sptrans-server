571e97438010781a66146055e99086d4797b52f2
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2017-06-27 20:54:10 -0300

    Update build

--- build/controllers/fares.js
+++ build/controllers/fares.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _helpers = require('../helpers');
 
-var file = './src/vendor/fares_attributes.txt';
+var file = './src/vendor/fare_attributes.txt';
 
 exports.default = function (req, res) {
   return (0, _helpers.csvToJson)(file).then(function (fares) {
